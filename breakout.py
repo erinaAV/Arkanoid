bfa4bb6aa06d88aa40da713850d8c2c24de94db8
Alina_EV <erina.av@yandex.ru>
2019-02-26 13:31:33 +0300

    Перемещение ракетки теперь удобнее. Добавлена заставка.

--- breakout.py
+++ breakout.py
@@ -13,6 +13,37 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 
+def terminate():
+    pygame.quit()
+    
+    
+def start_game():
+    text = ['ПРАВИЛА ИГРЫ:', 'Для победы необходимо разбить стену,', 'состоящую из блоков,',
+            'отражая ракеткой шарик.', 'Вы проиграли, если шарик улетел вниз.', ' ',
+            'Нажмите пробел, чтобы начать игру.']
+    
+    screen.fill((255, 255, 225))
+    font = pygame.font.Font(None, 30)
+    text_coord = 150   
+    for line in text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+        
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.update()      
+    
+
 class Brick(pygame.sprite.Sprite):
     def __init__(self, group, coords):
         super().__init__(all_sprites)
@@ -36,34 +67,46 @@ class Brick(pygame.sprite.Sprite):
         #else:
             #self.color = colors[self.strength]
             
-        
-class Board(pygame.sprite.Sprite):
-    pass
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, x, y, radius, demage):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites)     
         self.add(all_sprites)
         self.radius = radius
         self.demage = demage
         self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
         pygame.draw.circle(self.image, pygame.Color("red"), (radius, radius), radius)
         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = random.randint(-5, 5)
-        self.vy = random.randrange(-5, 5)
+        self.x = self.rect.topright[0]
+        self.y = self.rect.topright[1]          
+        self.vx = 1
+        self.vy = 3
         
-    def update(self):
+    def update(self):          
         self.rect = self.rect.move(self.vx, self.vy)
+        self.x = self.rect.topright[0]
+        self.y = self.rect.topright[1]        
         #if pygame.sprite.spritecollideany(self, horizontal_borders):
             #self.vy = -self.vy
         #if pygame.sprite.spritecollideany(self, vertical_borders):
             #self.vx = -self.vx
+        # столкновение с кирпичиками
         if pygame.sprite.spritecollideany(self, brickes):
-                self.vx = -self.vx 
+                self.vx = self.vx 
                 self.vy = -self.vy
+                print('brick')
+        # столкновение с доской
         if pygame.sprite.spritecollideany(self, paddle_group):
-                self.vx = -self.vx 
-                self.vy = -self.vy        
+                self.vx = self.vx 
+                self.vy = -self.vy
+                print('paddle')
+        # столкновение с границами игрового поля
+        #print(self.radius, self.rect.topright, width, height)
+        if not (self.radius <= self.x <= width):
+            self.vx = -self.vx
+            print('pole')
+        #if self.radius <= self.y >= height:
+            #self.vy = -self.vy        
 
 
 class Paddle(pygame.sprite.Sprite):
@@ -77,17 +120,31 @@ class Paddle(pygame.sprite.Sprite):
         
     def move_right(self):
         if self.rect.topleft[0] + 100 < width:
-            self.rect.topright = (self.rect.topright[0] + 5, self.rect.topright[1])
+            #self.rect.topright = (self.rect.topright[0] + 5, self.rect.topright[1])
+            self.rect.move_ip(5, 0)
                   
     def move_left(self):
         if self.rect.topleft[0] > 0:
-            self.rect.topright = (self.rect.topright[0] - 5, self.rect.topright[1])
-        
+            #self.rect.topright = (self.rect.topright[0] - 5, self.rect.topright[1])
+            self.rect.move_ip(-5, 0)
+
+# вылетел ли шарик за края поля
+def ball_fly_out(ball, coords):
+    x = coords[0]
+    y = coords[1]
+    if 0 <= x >= width:
+        ball.vx = -ball.vx
+    if 0 <= y >= heigth:
+        ball.vy = -ball.vy
+    
+start_game()
 
 all_sprites = pygame.sprite.Group()
 brickes = pygame.sprite.Group()
 paddle_group = pygame.sprite.Group()
 brickes.add(all_sprites)
+
+# создаем и рисуем спрайты
 for i in range(0, 594, 54):
     for j in range(0, 200, 25):
         Brick(brickes, (i, j))
@@ -95,9 +152,8 @@ paddle = Paddle()
 
 main_ball = Ball(400, 400, 10, 1)
 all_sprites.draw(screen)
-#brickes.draw(screen)
 pygame.display.update()
-#clock = pygame.time.Clock()
+
 
 p_moving_r = False
 p_moving_l = False
